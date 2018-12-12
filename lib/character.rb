f5ca54d52ee4e4e8f16c078aabadeb53f170bc54
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-12 10:38:14 -0700

    fixes indentation and deletes require pry

--- lib/character.rb
+++ lib/character.rb
@@ -1,12 +1,13 @@
 class Character
 
-attr_reader :name,
-            :actor,
-            :salary
-            
-def initialize(character_info)
-  @name = character_info[:name]
-  @actor = character_info[:actor]
-  @salary = character_info[:salary]
-end
+  attr_reader :name,
+              :actor,
+              :salary
+
+  def initialize(character_info)
+    @name = character_info[:name]
+    @actor = character_info[:actor]
+    @salary = character_info[:salary]
+  end
+  
 end
