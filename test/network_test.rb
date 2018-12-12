ea4bda81c54eefbd1b7257b8bd75044c17f2417c
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-12 10:02:40 -0700

    Adds final payroll test for network

--- test/network_test.rb
+++ test/network_test.rb
@@ -3,32 +3,32 @@ require 'minitest/pride'
 require './lib/character'
 require './lib/show'
 require './lib/network'
+require 'pry'
 
 class NetworkTest < MiniTest::Test
 
 def test_it_exists
-  skip
+
   nbc = Network.new("NBC")
 
-  assert_instance_of Network, network
+  assert_instance_of Network, nbc
 end
 
 def test_it_has_a_name
-  skip
+
   nbc = Network.new("NBC")
 
-  assert_equal "NBC", network.name
+  assert_equal "NBC", nbc.name
 end
 
 def test_it_starts_with_zero_shows
-  skip
   nbc = Network.new("NBC")
 
-  assert_equal [], network.shows
+  assert_equal [], nbc.shows
 end
 
 def test_it_can_add_shows
-  skip
+
   nbc = Network.new("NBC")
 
   michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
@@ -45,8 +45,8 @@ def test_it_can_add_shows
   assert_equal [knight_rider, parks_and_rec], nbc.shows
 end
 
-def it_can_calculate_highest_paid_actor
-  skip
+def test_it_can_calculate_highest_paid_actor
+
   nbc = Network.new("NBC")
 
   michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
@@ -62,6 +62,25 @@ def it_can_calculate_highest_paid_actor
 
   assert_equal "Amy Poehler", nbc.highest_paid_actor
 
+end
+
+def test_it_has_payroll
+  nbc = Network.new("NBC")
+
+  michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+  kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+  leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+  ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+  parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+  nbc.add_show(knight_rider)
+  nbc.add_show(parks_and_rec)
+
+  expected = {"David Hasselhoff" => 1600000, "William Daniels" => 1000000, "Amy Poehler" => 2000000, "Nick Offerman" => 1400000}
+
+  assert_equal expected, nbc.payroll
 end
 
 end
