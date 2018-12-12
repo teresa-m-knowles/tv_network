c08e9b786044865b76a9f397686b42edce7b3e04
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-12 09:27:11 -0700

    Creates total_salary method and finishes the show class

---
+++ lib/show.rb
@@ -0,0 +1,27 @@
+require './lib/character'
+require 'pry'
+
+class Show
+
+  attr_reader :name,
+              :creator,
+              :characters,
+              :total_salary
+
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    sum = 0
+    @characters.each do |character|
+      sum += character.salary
+    end
+
+    return sum
+
+  end
+end
